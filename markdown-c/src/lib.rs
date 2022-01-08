17b1d36e7537b04559f0be14275e9fbe1be8b24b
djkloop <1044032256@qq.com>
2022-01-08 17:29:37 +0800

    update

--- markdown-c/src/lib.rs
+++ markdown-c/src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use pulldown_cmark::{html, Options, Parser};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +10,17 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, markdown-c!");
+pub fn render_markdown(input_text: &str) -> String {
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(input_text, options);
+    // Write to String buffer.
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+    html_output
 }
