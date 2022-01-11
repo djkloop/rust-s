96bb3ea9c493324ea6da5392ca01f3dd6b1bf9e4
djkloop <1044032256@qq.com>
2022-01-12 01:31:54 +0800

    update sqlx add user

--- learn_login/src/entity/user.rs
+++ learn_login/src/entity/user.rs
@@ -1,8 +1,14 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Serialize)]
 pub struct User {
     pub id: Uuid,
     pub username: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateUser {
+    pub username: String,
+    pub password: String,
 }
--- learn_login/src/main.rs
+++ learn_login/src/main.rs
@@ -5,13 +5,17 @@ extern crate log;
 
 use std::env;
 
+use chrono::Utc;
+
+use argon2::Config;
 use dotenv::dotenv;
 use serde_json::json;
-use sqlx::{PgPool, Pool, query_as};
-use tide::{Server, Response, StatusCode, Request, Body};
+use sqlx::{query, query_as, PgPool, Pool};
+use tide::{Body, Request, Response, Server, StatusCode};
 
 mod entity;
 
+// 创建一个共有的 pg pool
 async fn make_db_pool() -> PgPool {
     let db_url = env::var("DATABASE_URL").unwrap();
     info!("make db pool with url -> {}", db_url);
@@ -27,18 +31,52 @@ async fn server() -> tide::Result<Server<State>> {
     dotenv().ok();
     pretty_env_logger::init();
     let db_pool = make_db_pool().await;
-    let state = State {
-        db_pool
-    };
+    let state = State { db_pool };
     let mut app = Server::with_state(state);
-    app.at("/users").get(|req: Request<State>| async move {
-        let db_share_pool = &req.state().db_pool;
-        // 获取到数据库里面所有的用户...
-        let users = query_as!(entity::user::User, "select id, username from users").fetch_all(db_share_pool).await?;
-        let mut res = Response::new(StatusCode::Ok);
-        res.set_body(Body::from_json(&json!({ "users": &users }))?);
-        Ok(res)
-    });
+    app.at("/users")
+        // get 获取所有用户
+        .get(|req: Request<State>| async move {
+            let db_share_pool = &req.state().db_pool;
+            // 获取到数据库里面所有的用户...
+            let users = query_as!(entity::user::User, "select id, username from users")
+                .fetch_all(db_share_pool)
+                .await?;
+            let mut res = Response::new(StatusCode::Ok);
+            res.set_body(Body::from_json(&json!({ "users": &users }))?);
+            Ok(res)
+        })
+        // 创建用户
+        .post(|mut req: Request<State>| async move {
+            let db_share_pool = req.state().db_pool.clone();
+            // 先从body里获取数据
+            let create_user = req.body_json::<entity::user::CreateUser>().await?;
+            // 然后把密码加一遍hash
+            let password = create_user.password.as_bytes();
+            let salt = env::var("SALT").unwrap();
+            let config = Config::default();
+            let hashed_password = argon2::hash_encoded(password, salt.as_bytes(), &config).unwrap();
+            // 存数据库
+            let row = query!(
+                r#"
+                    insert into users (id, username, hashed_password, create_at, updated_at) 
+                    values ($1, $2, $3, $4, $5);
+                "#,
+                uuid::Uuid::new_v4(),
+                create_user.username,
+                hashed_password,
+                Utc::now(),
+                Utc::now(),
+            )
+            .execute(&db_share_pool)
+            .await?;
+            info!("row affected {}", row.rows_affected());
+            let mut res = Response::new(StatusCode::Ok);
+            res.set_body(Body::from_json(&json!({
+                "affected": row.rows_affected(),
+                "message": "添加成功",
+            }))?);
+            Ok(res)
+        });
     Ok(app)
 }
 
